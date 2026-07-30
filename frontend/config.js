e7572f4ff1645f28e1800250f255651211a7b933
S7ven <yakabelmont@gmail.com>
2026-07-30 14:29:50 +0200

    feat: Retrieving and printing products with their data from the api

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,12 @@
 const config = {
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'http://localhost:3000',
+    currencyMap: new Map([
+        ['USD', '$'],
+        ['EUR', '€'],
+        ['GBP', '£'],
+        ['JPY', '¥']
+    ]),
+    discountDefault: 'No hay descuento'
 }
 
 export default config;
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -2,14 +2,35 @@ import config from './config.js'
 
 addEventListener('load', () => {
     getProducts()
+    .then(products => printProducts(products))
 })
 
-async function getProducts() {
-    console.log(config.apiUrl + '/products')
-    const result = await fetch(config.apiUrl + '/products')
+async function getProducts(numberPage=1) {
+    const result = await fetch(config.apiUrl + '/products' + '?numberPage=' + numberPage)
     const data = await result.json()
-    // console.log('data: ' + JSON.stringify(data))
-    for (const product of data) {
-        console.log('product: ' + JSON.stringify(product))
+    return data
+}
+
+function printProducts(products) {
+    let innerHTML = ''
+    let cardContainer = document.querySelector('#main_card_section_container')
+    for (const product of products) {
+        innerHTML += `
+        <div class="card_container">
+            <div class="card_container_image">
+                <img src="${config.apiUrl}${product.image}" alt="${product.name}">
+            </div>
+            <div class="card_container_info">
+                <ul>
+                    <li class="li_name"><span class="card_header">Name: </span><span class="card_info">${product.name}</span></li>
+                    <li class="li_price"><span class="card_header">Price: </span><span class="card_info">${product.price}${config.currencyMap.get(product.currency)}</span></li>
+                    <li class="li_discount"><span class="card_header">Discount: </span><span class="card_info">${product.discount > 0 ? `${product.discount}%` : config.discountDefault}</span></li>
+                    <li class="li_stock"><span class="card_header">Stock: </span><span class="card_info">${product.stock}</span></li>
+                    <li class="li_description"><span class="card_header">Description: </span><span class="card_info">${product.description}</span></li>
+                </ul>
+            </div>
+        </div>
+        `
     }
+    cardContainer.innerHTML = innerHTML
 }
