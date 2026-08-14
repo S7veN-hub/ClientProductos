c6b02a5d584b2cd41c5cbc40d4b87b54dbd20c35
S7ven <yakabelmont@gmail.com>
2026-08-14 14:11:56 +0200

    feat: Adding login and register pages and api connection with login and register routes

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -6,44 +6,126 @@ window.addEventListener('load', () => {
 })
 
 let navSearch = document.querySelector('#nav_search')
-navSearch.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        let productName = navSearch.value
-        console.log('Search for: ' + productName)
-        if (productName === '') {
-            globalNumberPage = 1
-            retrievingProducts(globalNumberPage)
-        } else {
-            globalNumberPage = 1
-            retrievingProductsByName(productName, globalNumberPage)
+if (navSearch) {
+    navSearch.addEventListener('keydown', event => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            let productName = navSearch.value
+            console.log('Search for: ' + productName)
+            if (productName === '') {
+                globalNumberPage = 1
+                retrievingProducts(globalNumberPage)
+            } else {
+                globalNumberPage = 1
+                retrievingProductsByName(productName, globalNumberPage)
+            }
         }
-    }
-})
+    })
+}
 
 let navPaginationPrev = document.querySelector('#nav_pagination_prev')
-navPaginationPrev.addEventListener('click', event => {
+if (navPaginationPrev) {
+    navPaginationPrev.addEventListener('click', event => {
+        event.preventDefault();
+        --globalNumberPage
+        let productName = navSearch.value
+        if (productName === '') {
+            retrievingProducts(globalNumberPage)
+        } else {
+            retrievingProductsByName(productName, globalNumberPage)
+        }
+    })
+}
+
+let navPaginationNext = document.querySelector('#nav_pagination_next')
+if (navPaginationNext) {
+    navPaginationNext.addEventListener('click', event => {
+        event.preventDefault();
+        ++globalNumberPage
+        let productName = navSearch.value
+        if (productName === '') {
+            retrievingProducts(globalNumberPage)
+        } else {
+            retrievingProductsByName(productName, globalNumberPage)
+        }
+    })
+}
+
+let formRegister = document.forms['form_register']
+let formRegisterInputUsername = document.forms['form_register']? document.forms['form_register']['Username'] : null
+let formRegisterInputEmail = document.forms['form_register']? document.forms['form_register']['Email'] : null
+let formRegisterInputPassword = document.forms['form_register']? document.forms['form_register']['Password'] : null
+let formRegisterInputConfirmPassword = document.forms['form_register']? document.forms['form_register']['ConfirmPassword'] : null
+let errorMessage1 = document.querySelector('#error_message1')
+let errorMessage2 = document.querySelector('#error_message2')
+let formRegisterButton = document.querySelector('#form_register button')
+
+if (formRegisterInputUsername) formRegisterInputUsername.addEventListener('input', validateRegisterForm)
+if (formRegisterInputEmail) formRegisterInputEmail.addEventListener('input', validateRegisterForm)
+if (formRegisterInputPassword) formRegisterInputPassword.addEventListener('input', validateRegisterForm)
+if (formRegisterInputConfirmPassword) formRegisterInputConfirmPassword.addEventListener('input', validateRegisterForm)
+if (formRegister) formRegister.addEventListener('submit', async event => {
     event.preventDefault();
-    --globalNumberPage
-    let productName = navSearch.value
-    if (productName === '') {
-        retrievingProducts(globalNumberPage)
+    const email = formRegisterInputEmail.value
+    const username = formRegisterInputUsername.value
+    const password = formRegisterInputPassword.value
+    const role = 'user'
+    const response = await fetch(config.apiUrl + '/register/check_user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            name: username
+        })
+    })
+    const data = await response.json()
+    if (data.exists) {
+        errorMessage2.style.display = 'block'
     } else {
-        retrievingProductsByName(productName, globalNumberPage)
+        errorMessage2.style.display = 'none'
+        const response2 = await fetch(config.apiUrl + '/register/register_user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                name: username,
+                password: password,
+                role: role
+            })
+        })
+        const data2 = await response2.json()
     }
 })
 
-let navPaginationNext = document.querySelector('#nav_pagination_next')
-navPaginationNext.addEventListener('click', event => {
+let formLogin = document.forms['form_login']
+let formLoginInputUsername = document.forms['form_login']? document.forms['form_login']['Username'] : null
+let formLoginInputPassword = document.forms['form_login']? document.forms['form_login']['Password'] : null
+let formLoginButton = document.querySelector('#form_login button')
+
+if (formLogin) formLogin.addEventListener('submit', async event => {
     event.preventDefault();
-    ++globalNumberPage
-    let productName = navSearch.value
-    if (productName === '') {
-        retrievingProducts(globalNumberPage)
-    } else {
-        retrievingProductsByName(productName, globalNumberPage)
-    }
+    const username = formLoginInputUsername.value
+    const password = formLoginInputPassword.value
+    const response = await fetch(config.apiUrl + '/login/login_user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: username,
+            password: password
+        })
+    })
+    const data = await response.json()
+    console.log(data)
 })
+if (formLoginInputUsername) formLoginInputUsername.addEventListener('input', validateLoginForm)
+if (formLoginInputPassword) formLoginInputPassword.addEventListener('input', validateLoginForm)
+
 
 function retrievingProducts(globalNumberPage) {
     getProducts(globalNumberPage)
@@ -80,47 +162,74 @@ async function getProductsByName(productName, numberPage) {
 function printProducts(products) {
     let innerHTML = ''
     let cardContainer = document.querySelector('#main_card_section_container')
-    for (const product of products) {
-        innerHTML += `
-        <div class="card_container">
-            <div class="card_container_image">
-                <img src="${config.apiUrl}${product.image}" alt="${product.name}">
+    if (cardContainer) {
+        for (const product of products) {
+            innerHTML += `
+            <div class="card_container">
+                <div class="card_container_image">
+                    <img src="${config.apiUrl}${product.image}" alt="${product.name}">
+                </div>
+                <div class="card_container_info">
+                    <ul>
+                        <li class="li_name"><span class="card_header">Name: </span><span class="card_info">${product.name}</span></li>
+                        <li class="li_price"><span class="card_header">Price: </span><span class="card_info">${product.price}${config.currencyMap.get(product.currency)}</span></li>
+                        <li class="li_discount"><span class="card_header">Discount: </span><span class="card_info">${product.discount > 0 ? `${product.discount}%` : config.discountDefault}</span></li>
+                        <li class="li_stock"><span class="card_header">Stock: </span><span class="card_info">${product.stock}</span></li>
+                        <li class="li_description"><span class="card_header">Description: </span><span class="card_info">${product.description}</span></li>
+                    </ul>
+                </div>
             </div>
-            <div class="card_container_info">
-                <ul>
-                    <li class="li_name"><span class="card_header">Name: </span><span class="card_info">${product.name}</span></li>
-                    <li class="li_price"><span class="card_header">Price: </span><span class="card_info">${product.price}${config.currencyMap.get(product.currency)}</span></li>
-                    <li class="li_discount"><span class="card_header">Discount: </span><span class="card_info">${product.discount > 0 ? `${product.discount}%` : config.discountDefault}</span></li>
-                    <li class="li_stock"><span class="card_header">Stock: </span><span class="card_info">${product.stock}</span></li>
-                    <li class="li_description"><span class="card_header">Description: </span><span class="card_info">${product.description}</span></li>
-                </ul>
-            </div>
-        </div>
-        `
+            `
+        }
+        cardContainer.innerHTML = innerHTML
     }
-    cardContainer.innerHTML = innerHTML
 }
 
 function checkPagination(numberPage, data) {
     if (numberPage <= 1) {
         let prevButton = document.querySelector('#nav_pagination_prev')
-        prevButton.className = 'nav_pagination_item_disabled'
+        if (prevButton) prevButton.className = 'nav_pagination_item_disabled'
         if (data.length > 0) {
             let nextButton = document.querySelector('#nav_pagination_next')
-            nextButton.className = 'nav_pagination_item'
+            if (nextButton) nextButton.className = 'nav_pagination_item'
         } else {
             let nextButton = document.querySelector('#nav_pagination_next')
-            nextButton.className = 'nav_pagination_item_disabled'
+            if (nextButton) nextButton.className = 'nav_pagination_item_disabled'
         }
     } else {
         let prevButton = document.querySelector('#nav_pagination_prev')
-        prevButton.className = 'nav_pagination_item'
+        if (prevButton) prevButton.className = 'nav_pagination_item'
         if (data.length > 0) {
             let nextButton = document.querySelector('#nav_pagination_next')
-            nextButton.className = 'nav_pagination_item'
+            if (nextButton) nextButton.className = 'nav_pagination_item'
         } else {
             let nextButton = document.querySelector('#nav_pagination_next')
-            nextButton.className = 'nav_pagination_item_disabled'
+            if (nextButton) nextButton.className = 'nav_pagination_item_disabled'
         }
+    }
+}
+
+function validateRegisterForm() {
+    console.log('Validating register form...')
+    if (formRegisterInputUsername.value !== '' && formRegisterInputEmail.value !== '' && formRegisterInputPassword.value !== '' && formRegisterInputConfirmPassword.value !== '') {
+        if (formRegisterInputPassword.value === formRegisterInputConfirmPassword.value) {
+            formRegisterButton.className = 'register_button'
+            errorMessage1.style.display = 'none'
+        } else {
+            formRegisterButton.className = 'register_button_disabled'
+            errorMessage1.style.display = 'block'
+        }
+    } else {
+        formRegisterButton.className = 'register_button_disabled'
+        errorMessage1.style.display = 'none'
+    }
+}
+
+function validateLoginForm() {
+    console.log('Validating login form...')
+    if (formLoginInputUsername.value !== '' && formLoginInputPassword.value !== '') {
+        formLoginButton.className = 'login_button'
+    } else {
+        formLoginButton.className = 'login_button_disabled'
     }
 }
